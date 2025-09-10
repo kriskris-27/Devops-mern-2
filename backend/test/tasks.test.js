4d2a0ebe34276d17a33809f80e328f76ae41f7d0
Kris <krishnakumars0101@gmail.com>
2025-09-11 01:29:45 +0530

    CI workflow using shell script

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -11,6 +11,8 @@ expect(res.statusCode).toBe(200)
 it('should return array data and tasks property', async () => {
     const res = await request(app).get('/api/tasks')
     expect(typeof res.body).toBe('object')
+    console.log(res.body.tasks, 'DATA SEEDED');
+    
     expect(res.body).toHaveProperty("tasks")
     })
 
