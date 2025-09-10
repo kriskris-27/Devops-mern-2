d62fbc6f201427cb5859a7d0cf85a69cdd89ebf1
Kris <krishnakumars0101@gmail.com>
2025-09-10 22:40:28 +0530

    test written for GET method and type of data receiving

---
+++ backend/test/tasks.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const { app, server } = require('../index')
+const mongoose = require('mongoose')
+
+describe('GET /api/tasks', () => {
+it('should return 200 OK', async () => {
+const res = await request(app).get('/api/tasks')
+expect(res.statusCode).toBe(200)
+})
+it('should return array data', async () => {
+    const res = await request(app).get('/api/tasks')
+    expect(typeof res.body).toBe(object)
+    })
+
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+    await server.close()
+
+    })
