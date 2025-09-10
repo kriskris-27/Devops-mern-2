42fa13d1b68809c89e19ccdcc0403b212550a6c5
Kris <krishnakumars0101@gmail.com>
2025-09-10 22:47:01 +0530

    tested about res.body to Have Property

--- backend/test/tasks.test.js
+++ backend/test/tasks.test.js
@@ -7,9 +7,11 @@ it('should return 200 OK', async () => {
 const res = await request(app).get('/api/tasks')
 expect(res.statusCode).toBe(200)
 })
-it('should return array data', async () => {
+
+it('should return array data and tasks property', async () => {
     const res = await request(app).get('/api/tasks')
-    expect(typeof res.body).toBe(object)
+    expect(typeof res.body).toBe('object')
+    expect(res.body).toHaveProperty("tasks")
     })
 
 })
